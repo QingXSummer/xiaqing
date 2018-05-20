181d8ed394891e69d30246ddb2caba75031d3efb
QingX <QingX@DESKTOP-5UHIO34>
2018-05-20 13:07:18 +0800

    1

--- src/main/java/test/test/StringObject.java
+++ src/main/java/test/test/StringObject.java
@@ -1,15 +1,17 @@
-package test.test;
+package test.test;import java.sql.Date;
 
 public class StringObject {
 	
-	String s1="";
-	String s2=new String("123")+new String("456");
-	String s3=new String("123")+new String("456");
 	
 	public static void main(String[] args) {
-		StringObject object = new StringObject();
-		System.out.println(object.s1 == object.s2);
-		System.out.println(object.s2 == object.s2.intern());
-		System.out.println(object.s2 == object.s3.intern());
+//		StringObject object = new StringObject();
+//		System.out.println(object.s1 == object.s2);
+//		System.out.println(object.s2 == object.s2.intern());
+//		System.out.println(object.s2 == object.s3.intern());
+		
+		
+		System.out.println(new Date(1526745600000L).toLocaleString());
+		System.out.println(new Date(1526518407293L).toLocaleString());
+		
 	}
 }
