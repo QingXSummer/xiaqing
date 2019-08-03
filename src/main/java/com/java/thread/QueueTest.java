3a22c585e6efbc99e0f9df2118340c5a34056fb0
QingX <xiaqing215400698@qq.com>
2019-08-03 09:35:38 +0800

    修改方法

--- src/main/java/com/java/thread/QueueTest.java
+++ src/main/java/com/java/thread/QueueTest.java
@@ -1,6 +1,7 @@
 package com.java.thread;
 
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * 描述    :
@@ -9,6 +10,25 @@ import java.util.concurrent.ConcurrentLinkedDeque;
  */
 public class QueueTest {
     public static void main(String[] args) {
+        Comparable comparable;
+        Comparator comparator;
         ConcurrentLinkedDeque linkedDeque;
+        CyclicBarrier cyclicBarrier;
+        CountDownLatch countDownLatch;
+        Semaphore semaphore;
+        Executor executor;
+        ExecutorService service;
+        Callable callable;
+        Executors.callable(()->System.out.println());
+        Executors.newFixedThreadPool(10);
+        SynchronousQueue synchronousQueue;
+        FutureTask futureTask;
+        ThreadFactory threadFactory;
+        LinkedBlockingQueue queue;
+        ThreadLocal local;
+        Iterator iterator;
+        ListIterator listIterator;
+        Collection collection;
+        Collections collections;
     }
 }
