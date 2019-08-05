55b2f0ff6fb3efb49818f3aec6d9541f5cdbe63a
QingX <xiaqing215400698@qq.com>
2019-08-05 19:05:43 +0800

    红黑树删除方法

--- src/main/java/com/algorithms/tree/RedBlackTree.java
+++ src/main/java/com/algorithms/tree/RedBlackTree.java
@@ -12,10 +12,20 @@ public class RedBlackTree<Key extends Comparable <Key>, Value> {
     public static void main(String[] args) {
         RedBlackTree <String, Object> redBlackTree = new RedBlackTree <>();
         redBlackTree.put("a", "a");
-        redBlackTree.put("b", "b");
         redBlackTree.put("c", "c");
+        redBlackTree.put("e", "e");
+//        redBlackTree.put("c", "c");
+        redBlackTree.put("h", "h");
+        redBlackTree.put("l", "l");
+        redBlackTree.put("m", "m");
+        redBlackTree.put("p", "p");
+        redBlackTree.put("r", "r");
+        redBlackTree.put("s", "s");
+        redBlackTree.put("x", "x");
         System.out.println(redBlackTree.size());
         System.out.println(redBlackTree.get("d"));
+        System.out.println(redBlackTree.get("e"));
+        System.out.println(redBlackTree.get("a"));
     }
 
     /**
@@ -29,7 +39,7 @@ public class RedBlackTree<Key extends Comparable <Key>, Value> {
      * Date   :2019-51-13 21:51:22
      */
     private class Node {
-        private Node parent,left,right;
+        private Node parent, left, right;
         private Key key;
         private Value value;
         private int N;
@@ -40,6 +50,16 @@ public class RedBlackTree<Key extends Comparable <Key>, Value> {
             this.value = v;
             this.isRed = true;
             N = 1;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    ", N=" + N +
+                    ", isRed=" + isRed +
+                    '}';
         }
     }
 
@@ -72,54 +92,75 @@ public class RedBlackTree<Key extends Comparable <Key>, Value> {
             root.isRed = false;
             return;
         }
-        root = put(null, root, key, value);
+        put(root, key, value);
         root.isRed = false;
     }
 
-    public Node put(Node parent, Node node, Key key, Value value) {
-        if (node == null) {
-            node = new Node(key, value);
-            node.parent = parent;
-        } else {
-            int cmp = node.key.compareTo(key);
-            if (cmp < 0) {
-                node.right = put(node, node.right, key, value);
-                node.N=size(node.left)+size(node.right)+1;
+    public Node put(Node node, Key key, Value value) {
+        Node l, t = node;
+        boolean isLeft = false;
+        boolean sizeInc = get(key) == null ? false : true;
+        while (true) {
+            int cmp = key.compareTo(node.key);
+            l = t;
+            if (cmp == 0) {
+                t.value = value;
+                return node;
             } else if (cmp > 0) {
-                node.left = put(node, node.left, key, value);
-                node.N=size(node.left)+size(node.right)+1;
+                if (!sizeInc)
+                    t.N = t.N + 1;
+                t = t.right;
+                isLeft = false;
             } else {
-                node.value = value;
+                if (!sizeInc)
+                    t.N = t.N + 1;
+                t = t.left;
+                isLeft = true;
             }
-
+            if (t == null)
+                break;
         }
-        //父亲节点为黑色，直接插入
-        if (node.parent == null || !node.parent.isRed) {
+        Node newN = new Node(key, value);
+        newN.parent = l;
+        if (isLeft)
+            l.left = newN;
+        else
+            l.right = newN;
+        //父亲节点为黑色或为根节点，直接插入
+        Node p = newN.parent;
+        if (p == null || !isRed(p)) {
             return node;
         }
-        //当前节点和父节点都为红色
-         if (node.isRed && node.parent.isRed) {
-            //父节点为左节点且当前节点为右节点，将当前节点挪到左节点上
-            if (node.parent.parent.left == node.parent && node.parent.parent.right == null) {
-                if (node.parent.right == node) {
-                    node.parent.left = node;
-                    node.parent.right = null;
-                }
-                rightRotate(node.parent.parent);
-            } else if (node.parent.parent.right == node.parent && node.parent.parent.left == null) {
-                if (node.parent.left == node) {
-                    node.parent.right = node;
-                    node.parent.left = null;
-                }
-                leftRotate(node.parent.parent);
-            } else if (node.parent.parent.left.isRed && node.parent.parent.right.isRed) {
-                node.parent.parent.left.isRed = false;
-                node.parent.parent.right.isRed = false;
-                node.parent.parent.isRed = true;
-            }
-        }
+        fixBalance(newN);
         return node;
     }
+
+    //修复红黑树平衡关系
+    private void fixBalance(Node node) {
+        Node pa, unc;
+        while (isRed(pa = node.parent)) {
+            Node gp = pa.parent;
+            unc = getUncle(node);
+            if (isRed(unc)) {
+                pa.isRed = false;
+                unc.isRed = false;
+                gp.isRed = true;
+                node = gp;
+                continue;
+            }
+            if (isLeft(pa)) {
+                if (!isLeft(node))
+                    gp.left = leftRotate(pa);
+                rightRotate(gp);
+            } else {
+                if (isLeft(node))
+                    rightRotate(pa);
+                leftRotate(gp);
+            }
+        }
+        this.root.isRed = false;
+    }
+
 
     /**
      * 描述    :对节点进行左旋
@@ -127,27 +168,133 @@ public class RedBlackTree<Key extends Comparable <Key>, Value> {
      * Date   :2019-26-31 17:26:38
      */
     private Node leftRotate(Node node) {
+        Node p = node.parent;
         Node r = node.right;
+        r.parent = p;
         boolean color = node.isRed;
         node.isRed = r.isRed;
         r.isRed = color;
+        if (r.left != null) {
+            r.left.parent = node;
+        }
         node.right = r.left;
         node.N = size(node.left) + size(node.right) + 1;
+        if (p == null) {
+            root = r;
+        } else if (isLeft(node)) {
+            p.left = r;
+        } else {
+            p.right = r;
+        }
+        node.parent = r;
         r.left = node;
         r.N = size(r.left) + size(r.right) + 1;
         return r;
     }
 
     private Node rightRotate(Node node) {
+        Node p = node.parent;
         Node l = node.left;
+        l.parent = p;
         boolean isRed = node.isRed;
         node.isRed = l.isRed;
         l.isRed = isRed;
+        if (l.right != null) {
+            l.right.parent = node;
+        }
         node.left = l.right;
         node.N = size(node.left) + size(node.right) + 1;
+        if (p == null) {
+            root = p;
+        } else if (isLeft(node)) {
+            p.left = l;
+        } else {
+            p.right = l;
+        }
+        node.parent = l;
         l.right = node;
         l.N = size(l.left) + size(l.right) + 1;
         return l;
+    }
+
+    /**
+     * 描述    :删除键值对应的key
+     * Author :QingX
+     * Date   :2019-04-05 17:04:34
+     */
+    public Value delete(Key key) {
+        Node node = deleteNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    private Node deleteNode(Node node, Key key) {
+        Node d = node;
+        while (true) {
+            int cmp = key.compareTo(d.key);
+            if (cmp == 0) {
+                break;
+            } else if (cmp > 0) {
+                d = d.right;
+            } else {
+                d = d.left;
+            }
+            if (d == null) {
+                return null;
+            }
+        }
+        return deleteNode(d);
+    }
+
+    private Node deleteNode(Node d){
+        if (d == null) return null;
+        Node p = d.parent;
+        //叶子节点
+        if (d.left == null && d.right == null) {
+
+        } else if (d.left == null) { // 有右节点
+            if (isLeft(d)) {
+                if (p != null)
+                    p.left = d.right;
+            } else {
+                if (p != null)
+                    p.right = d.right;
+            }
+            d.right.parent = p;
+            fixSize(d.right);
+        } else if (d.right == null) { //有左节点
+            if (isLeft(d)) {
+                if (p != null)
+                    p.left = d.left;
+            } else {
+                if (p != null)
+                    p.right = d.left;
+            }
+            d.left.parent = p;
+            fixSize(d.left);
+        } else { //同时拥有两个节点
+            Node t = d;
+            Node l = t;
+            while (t != null) {
+                l = t;
+                t = t.right;
+            }
+            d.value = l.value;
+            deleteNode(l);
+        }
+        return null;
+    }
+
+    private void fixSize(Node node) {
+        Node p = node;
+        do {
+            p.N = size(p.left) + size(p.right);
+            p = p.parent;
+        } while (p != null);
+    }
+
+    //判断节点是否为空色节点
+    private boolean isRed(Node node) {
+        return node == null ? false : node.isRed;
     }
 
     /**
@@ -164,7 +311,46 @@ public class RedBlackTree<Key extends Comparable <Key>, Value> {
      * Author :QingX
      * Date   :2019-35-31 17:35:24
      */
-    private int size() {
+    public int size() {
         return size(root);
+    }
+
+
+    /**
+     * 描述    : 判断节点是否为左子节点
+     * Author :QingX
+     * Date   :2019-01-05 09:01:13
+     */
+    private boolean isLeft(Node node) {
+        if (node.parent == null)
+            return false;
+        return node.parent.left == node ? true : false;
+    }
+
+    /**
+     * 描述    : 判断节点是否为右子节点
+     * Author :QingX
+     * Date   :2019-02-05 09:02:40
+     */
+    private boolean isRight(Node node) {
+        if (node == null || node.parent == null)
+            return false;
+        return node.parent.right == node ? true : false;
+    }
+
+    /**
+     * 描述    :获取叔叔节点 不存在返回null
+     * Author :QingX
+     * Date   :2019-03-05 10:03:11
+     */
+    private Node getUncle(Node node) {
+        Node pa = node.parent;
+        if (pa != null) {
+            Node ppa = pa.parent;
+            if (ppa != null) {
+                return ppa.left == pa ? ppa.right : ppa.left;
+            }
+        }
+        return null;
     }
 }
